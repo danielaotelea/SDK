7d9f277e3b33506565cc69512ce69560b851aacf
danielaotelea <daniela.otelea@gmail.com>
2023-04-05 15:03:29 +0200

    Release version: 1.1.11

---
+++ TrustPaymentsSdk.xcframework/ios-arm64_x86_64-simulator/TrustPaymentsSdk.framework/Headers/TrustPaymentsSdk.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for TrustPaymentsSdk.
+FOUNDATION_EXPORT double TrustPaymentsSdkVersionNumber;
+
+//! Project version string for TrustPaymentsSdk.
+FOUNDATION_EXPORT const unsigned char TrustPaymentsSdkVersionString[];
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrustPaymentsSdk/PublicHeader.h>
